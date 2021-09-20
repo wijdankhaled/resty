68c832e48bab7b6a0df3c8eb0f753ede9a534467
wijdankhaled <wijdankhaled178@gmail.com>
2021-09-20 20:21:33 +0300

    lab27

--- src/components/results/index.js
+++ src/components/results/index.js
@@ -1,12 +1,20 @@
 
+import JSONPretty from 'react-json-pretty';
+import 'react-json-pretty/themes/monikai.css';
+import Loading from '../loading/loading';
 
-function Results (props) {
-  
-    return (
-      <section>
-        <pre>{props.data ? JSON.stringify(props.data, undefined, 2) : null}</pre>
-      </section>
-    );
+function Results(props) {
+
+  return (
+    <section>
+      {props.data ? <div>
+        <h2>Headers</h2>
+        <JSONPretty id="json-pretty" data={props.data ? props.data.headers : null}></JSONPretty>
+        <h2>Results</h2>
+        <JSONPretty id="json-pretty" data={props.data ? props.data.data : null}></JSONPretty>
+      </div> : <Loading />}
+    </section>
+  );
 
 }
 
